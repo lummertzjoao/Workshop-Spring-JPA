c03d5ed9098a9d36cc80ccbdcbb68b6cfefe6815
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-11-10 03:50:34 -0300

    Project created

---
+++ src/main/java/com/github/lummertzjoao/workshopspringjpa/WorkshopSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.github.lummertzjoao.workshopspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringJpaApplication.class, args);
+	}
+
+}
