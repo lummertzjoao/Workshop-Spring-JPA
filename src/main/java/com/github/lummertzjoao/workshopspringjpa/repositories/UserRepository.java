08f7d3d43b408caaa734ccb5ce5f130e957d390c
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-11-10 18:17:21 -0300

    Service layer and component registration

--- src/main/java/com/github/lummertzjoao/workshopspringjpa/repositories/UserRepository.java
+++ src/main/java/com/github/lummertzjoao/workshopspringjpa/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.github.lummertzjoao.workshopspringjpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.github.lummertzjoao.workshopspringjpa.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
