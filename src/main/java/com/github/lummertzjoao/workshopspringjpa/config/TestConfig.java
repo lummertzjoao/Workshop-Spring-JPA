9cae840d7a088f664121db51fab62563ad402ca8
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-11-18 06:16:23 -0300

    Implemented Payment entity

--- src/main/java/com/github/lummertzjoao/workshopspringjpa/config/TestConfig.java
+++ src/main/java/com/github/lummertzjoao/workshopspringjpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.github.lummertzjoao.workshopspringjpa.entities.Category;
 import com.github.lummertzjoao.workshopspringjpa.entities.Order;
 import com.github.lummertzjoao.workshopspringjpa.entities.OrderItem;
+import com.github.lummertzjoao.workshopspringjpa.entities.Payment;
 import com.github.lummertzjoao.workshopspringjpa.entities.Product;
 import com.github.lummertzjoao.workshopspringjpa.entities.User;
 import com.github.lummertzjoao.workshopspringjpa.entities.enums.OrderStatus;
@@ -66,5 +67,8 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
